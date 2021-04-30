57d540148beedb5735d01ae485a0c4a3cc52017a
JoaquimLP <joaquimlp2013@gmail.com>
2021-04-30 19:31:25 -0300

    Fix

---
+++ src/configs/api.js
@@ -0,0 +1,3 @@
+
+export const URI_BASE_API = 'http://localhost:8000/api/v1'
+export const TOKEN_NAME = 'token_sanctum'
--- src/main.js
+++ src/main.js
@@ -1,9 +1,18 @@
+require ('./bootstrap')
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import store from './store'
 
 import 'bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import '@fortawesome/fontawesome-free/css/all.min.css';
 
-createApp(App).use(router).mount('#app')
+//Vue.config.productionTip = false
+
+const app = createApp(App)
+
+app.use(router)
+app.use(store)
+
+app.mount('#app')
